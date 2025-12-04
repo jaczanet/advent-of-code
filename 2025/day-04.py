44912c1b1503e98545511f88a5d83ab5131c3dd8
jaczanet <hi@jacza.net>
2025-12-04 18:40:10 +0200

    Simplifications

--- 2025/day-04.py
+++ 2025/day-04.py
@@ -1,24 +1,44 @@
 # https://adventofcode.com/2025/day/4
 
+from itertools import product
+
+
+class Matrix(list):
+    # Essentially a list of lists that enables indexing using tuples
+    # e.g. matrix[(x, y)]
+    # e.g. matrix[x, y]
+    # which are equivalent to matrix[x][y]
+
+    def __getitem__(self, index, /):
+        if isinstance(index, tuple):
+            if len(index) > 2:
+                raise ValueError("Index must be a tuple of exactly two elements (for 2D indexing).")
+            return super().__getitem__(index[0])[index[1]]
+        else:
+            return super().__getitem__(index)
+
+    def __setitem__(self, index, value, /):
+        if isinstance(index, tuple):
+            if len(index) > 2:
+                raise ValueError("Index must be a tuple of exactly two elements (for 2D indexing).")
+            self[index[0]][index[1]] = value
+        else:
+            super().__setitem__(index, value)
+
+
+Position = tuple[int]
+
 
 # Input
 
 with open('2025/day-04-input.txt') as file:
-    diagram = [list(line.strip()) for line in file]
+    diagram = Matrix(list(line.strip()) for line in file)
 
 
 # Solution
 
-from collections import namedtuple
-from itertools import product
-from itertools import starmap
-
-
-Position = namedtuple('Position', ['x', 'y'])
-
-
 cells = len(diagram)
-iswithinbounds = lambda pos: 0 <= pos.x < cells and 0 <= pos.y < cells
+iswithinbounds = lambda x, y: 0 <= x < cells and 0 <= y < cells
 
 
 def neighbours(center: Position) -> set[Position]:
@@ -27,57 +47,33 @@ def neighbours(center: Position) -> set[Position]:
     positions = {
         pos
         for dx, dy in product(range(-1, 2), repeat=2)
-        if iswithinbounds(pos := Position(x + dx, y + dy))
+        if iswithinbounds(*(pos := (x + dx, y + dy)))
     }
     positions.remove(center)
     return positions
 
 
-class Diagram(list):
-
-    def __getitem__(self, index, /):
-        if isinstance(index, tuple):
-            return super().__getitem__(index[0])[index[1]]
-        else:
-            return super().__getitem__(index)
-
-    def __setitem__(self, index, value, /):
-        if isinstance(index, tuple):
-            self[index[0]][index[1]] = value
-        else:
-            super().__setitem__(index, value)
-
-
-diagram = Diagram(diagram)
-
-
-def remove(diagram: Diagram) -> int:
+def remove(diagram: Matrix) -> int:
     """Edits the matrix *IN PLACE* and returns the amount of removed paper rools."""
-    count = 0
-    collecting = set()
+    collecting: set[Position] = set()
 
-    for pos in starmap(Position, product(range(cells), repeat=2)):
-        if (elem := diagram[pos]) == '.' or elem == 'x':
-            continue
+    for pos in product(range(cells), repeat=2):
+        if diagram[pos] == '@':
 
-        paperaround = [diagram[neighpos] for neighpos in neighbours(pos)].count('@')
-        if paperaround < 4:
-            collecting.add(pos)
-            count += 1
+            paperaround = [diagram[neighpos] for neighpos in neighbours(pos)].count('@')
+
+            if paperaround < 4:
+                collecting.add(pos)
 
     for pos in collecting:
         diagram[pos] = 'x'
 
-    return count
+    return len(collecting)
 
 
-collected = 0
-collected += remove(diagram)
-print('Silver solution:', collected)
+total = remove(diagram)
+print('Silver solution:', total)
 
-total = collected
-while collected:
-    collected = remove(diagram)
+while collected := remove(diagram):
     total += collected
-
 print('Gold solution:', total)
