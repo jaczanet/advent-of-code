ee0b9d37091a5ae9f7cd7347041647682eb197d9
jaczanet <hi@jacza.net>
2025-12-05 17:18:45 +0200

    Gold solution Day 5 - 2025 and improved complexity of silver solution

--- 2025/day-05.py
+++ 2025/day-05.py
@@ -19,11 +19,49 @@ inventory = tuple(map(int, ids.split('\n')))
 
 # Solution
 
-count = 0
-for id in inventory:
-    for idrange in freshranges:
-        if id in idrange:
-            count += 1
-            break
+# remove redundancy
+uniques = list()
+freshranges = sorted(freshranges, key=lambda r: (r.start, r.stop))
+prevstart = prevstop = -1
 
-print('Silver solution:', count)
+for idrange in freshranges:
+    currstart, currstop = idrange.start, idrange.stop
+
+    # merge
+    if currstart <= prevstop:
+        prevstop = max(prevstop, currstop)
+
+    else:
+        uniques.append(range(prevstart, prevstop))
+        prevstart, prevstop = currstart, currstop
+else:
+    # handle the final case
+    uniques.append(range(prevstart, prevstop))
+
+freshranges = uniques
+
+inventory = sorted(inventory)
+
+fresh = 0
+last = 0
+for id in inventory:
+    for j in range(last, len(freshranges)):
+        idrange = freshranges[j]
+        if id < idrange.start:
+            break
+        elif id in idrange:
+            fresh += 1
+            last = j
+            break
+        elif id >= idrange.stop:
+            last = j
+
+
+print('Silver solution:', fresh)
+
+
+total = 0
+for idrange in freshranges:
+    total += len(idrange)
+
+print('Gold solution:', total)
