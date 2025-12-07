070994af41bfb141a35e6c8841bfa7e03ab18c73
jaczanet <hi@jacza.net>
2025-12-08 01:48:54 +0200

    Changed from backtraking to dynamic programming

--- 2025/day-07.py
+++ 2025/day-07.py
@@ -1,8 +1,5 @@
 # https://adventofcode.com/2025/day/7
 
-from enum import Enum
-from functools import partial, cache
-
 
 # Input
 
@@ -11,75 +8,41 @@ with open('2025/day-07-input.txt') as file:
     diagram = file.read().strip()
 
 
-beam = int('0b' + start.replace('.', '0').replace('S', '1'), base=2)
+beam = int(start.replace('.', '0').replace('S', '1'), base=2)
 manifold = tuple(
-    int('0b' + scheme.replace('.', '0').replace('^', '1'), base=2)
+    int(scheme.replace('.', '0').replace('^', '1'), base=2)
     for scheme in diagram.split('\n')[1::2]
 )
 
 
 # Solution
 
+bits = len(start)
+quantum = [0] * bits
+quantum[start.index("S")] = 1
 
-class Direction(Enum):
-    LEFT = -1
-    RIGHT = 1
+splits = 0
 
-
-def traverse(beam: int, splitters: int, direction: Direction) -> int:
-    """Returns the beam after crossing the row of splitters, moving in the specified direction."""
+for splitters in manifold:
 
     hits = beam & splitters
-
-    match direction:
-        case Direction.LEFT:
-            new = hits << 1
-        case Direction.RIGHT:
-            new = hits >> 1
-        case _:
-            raise ValueError("direction must be Direction.LEFT or Direction.RIGHT")
+    new = (hits << 1) | (hits >> 1)  # left and right shift
 
     beam &= ~splitters  # keep untouched beams
-    beam |= new  # add new split beams
+    beam |= new  # add new splitted beams
 
-    return beam
+    mask = bin(hits)[2:].rjust(bits)
+    for i, bit in enumerate(mask):
+        if bit == '1':
 
+            t = quantum[i]
+            quantum[i - 1] += t
+            quantum[i + 1] += t
+            quantum[i] = 0
 
-def splits(beam: int, manifold) -> int:
-    """Returns the total number of times a tachyon beam is split in a manifold."""
-    splits = 0
-    for splitters in manifold:
+            splits += 1
 
-        splits += bin(beam & splitters).count('1')  # count of hits
+timelines = sum(quantum)
 
-        # combined new beam after crossing the row of splitters
-        left = traverse(beam, splitters, Direction.LEFT)
-        right = traverse(beam, splitters, Direction.RIGHT)
-        beam = left | right
-
-    return splits
-
-
-def timelines(beam, manifold) -> int:
-    """Returns the number of different timelines a beam traversing the manifold may end up in."""
-
-    n = len(manifold)
-
-    @cache
-    def _timelines(ray, level):
-        # end of backtracking
-        if level == n:
-            return 1
-
-        left = traverse(ray, manifold[level], Direction.LEFT)
-        right = traverse(ray, manifold[level], Direction.RIGHT)
-
-        uniquerays = set((left, right))
-
-        return sum(map(partial(_timelines, level=level + 1), uniquerays))
-
-    return _timelines(beam, level=0)
-
-
-print('Silver solution:', splits(beam, manifold))
-print('Gold solution:', timelines(beam, manifold))
+print('Silver solution:', splits)
+print('Gold solution:', timelines)
