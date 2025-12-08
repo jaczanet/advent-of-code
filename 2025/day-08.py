802c7857dbf2afac55ed78dc388df55bce538de7
jaczanet <hi@jacza.net>
2025-12-08 16:40:10 +0200

    Silver soltution Day 8 - 2025

---
+++ 2025/day-08.py
@@ -0,0 +1,48 @@
+# https://adventofcode.com/2025/day/8
+
+from collections import namedtuple, Counter
+from math import sqrt, prod
+
+
+Point = namedtuple('Point', ['x', 'y', 'z'])
+Connection = namedtuple('Connection', ['i', 'j', 'distance'])
+
+
+# Constants
+
+n = 1000  # total number of connections to perform
+m = 3  # total number of largest circuits to be considered
+
+
+# Input
+
+with open('2025/day-08-input.txt') as file:
+    jboxs = tuple(Point(*map(int, coords.split(','))) for coords in file.read().strip().split('\n'))
+
+
+# Solution
+
+euclideandistance = lambda p, q: sqrt(sum((pcoord - qcoord) ** 2 for pcoord, qcoord in zip(p, q, strict=True)))
+
+adjacencymatrix = tuple(tuple(euclideandistance(p, q) for q in jboxs) for p in jboxs)
+
+edges = [
+    Connection(i, j, distance)
+    for i, row in enumerate(adjacencymatrix)
+    for j, distance in enumerate(row[i:], start=i)
+    if distance
+]
+edges.sort(key=lambda connection: connection.distance)
+
+parent = list(range(len(jboxs)))  # track the root jbox for every circuit
+
+# perform connection for first n (shortest) edges
+for i, j, distance in edges[:n]:
+    i = parent[i]
+    j = parent[j]
+    # substitute the root of j with the root of i
+    parent = [p if p != j else i for p in parent]
+
+circuits = Counter(parent)
+
+print('Silver solution:', prod(size for root, size in circuits.most_common(m)))
